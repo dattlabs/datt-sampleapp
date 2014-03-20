b2670f76a789ffd2bf0aef1d0618a8bcb90c02c1
Jonathan Leonard <johanatan@gmail.com>
2014-03-19 22:50:53 -0700

    Remove comments intended for neophytes.

--- src/main/scala/com/datt/sampleapp/Boot.scala
+++ src/main/scala/com/datt/sampleapp/Boot.scala
@@ -5,10 +5,8 @@ import akka.io.IO
 import spray.can.Http
 
 object Boot {
-  // we need an ActorSystem to host our application in
   implicit val system = ActorSystem("sample-app-system")
 
-  // create and start our service actor
   val service = system.actorOf(Props[ServiceActor], "ip-service")
 
   def main(args: Array[String]): Unit = {
