0b0583ae5752d08e815f5c4f3e1958a1e5fa8c0a
John Albietz <inthecloud247@gmail.com>
2014-03-21 11:11:37 -0700

    added first version of debug run command. fixed run files.

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-source build
+# not building first. assuming that it's built.
 
 docker run -i -t --rm -w "/files" --name "sampleapp" --hostname "sampleapp" datt/datt-sampleapp bash -c "supervisord; /bin/bash"
