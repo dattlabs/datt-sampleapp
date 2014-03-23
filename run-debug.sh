298d8348d2a509fa34784300ec3fd2642edf77a6
John Albietz <inthecloud247@gmail.com>
2014-03-23 11:21:18 -0700

    meta-updated run and run-debug commands.

--- run-debug.sh
+++ run-debug.sh
@@ -2,4 +2,4 @@
 
 source build
 
-docker run -i -t --rm -w "/files" --name "sampleapp" --hostname "sampleapp" datt/datt-sampleapp bash -c "supervisord; /bin/bash"
+docker run -i -t --rm -w "/files" --name "$CURRENT_DIR" --hostname $CURRENT_DIR loyal3/$CURRENT_DIR bash -c "supervisord; /bin/bash"
